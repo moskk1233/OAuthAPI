dcb476259e6e56f68a241ae2c720344d211db261
Nonpawit Pothinil <mosaggpothinil@gmail.com(Nonpawit Pothinil)>
2023-08-29 13:57:23 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+    res.send('<a href="/auth/google">Authentication With Google </a>')
+});
+
+app.get('')
+
+app.listen(PORT, () => {
+    console.log(`Listen to port ${PORT}`)
+});
