64d61a8b0c31c683d2eb7c3c69adbd7963d8698b
Nonpawit Pothinil <mosaggpothinil@gmail.com(Nonpawit Pothinil)>
2023-08-29 16:03:24 +0700

    Add login with facebook

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
 const express = require("express");
-const path = require("path");
+const expressSession = require("express-session");
+const passport = require("passport");
+const FacebookStrategy = require("passport-facebook").Strategy;
 
 const app = express();
 const PORT = 5000;
 
+const FACEBOOK_APP_ID = "Input_FACEBOOK_APP_ID"
+const FACEBOOK_APP_SECRET = "Input_FACEBOOK_APP_SECRET"
 
 
-app.get("/", (req, res) => {
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "/facebook",
+    profileFields: ['emails', 'displayName', 'name', 'picture']
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    cb(null, profile)
+  }
+));
 
-    res.send('<a href="/auth/google">Authentication With Google </a>')
-});
+passport.serializeUser((user, callback) => {
+    callback(null, user);
+})
 
-app.get('')
+passport.deserializeUser((user, callback) => {
+    callback(null, user)
+})
+
+app.use(expressSession({
+    secret: 'testsystemnaja',
+    resave: true,
+    saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+// Route
+
+app.get('/login/facebook', passport.authenticate('facebook'))
+app.get('/facebook', passport.authenticate('facebook'), (req, res) => {
+    res.redirect('/')
+})
+
+app.get('/', (req, res) => {
+    res.send(req.user ? req.user : 'Not login with Facebook\n<a href="/login/facebook">Login with Facebook</a>')
+})
+
+app.get('/logout', (req, res) => {
+    req.logout((err) => {});
+    res.redirect('/')
+})
 
 app.listen(PORT, () => {
-    console.log(`Listen to port ${PORT}`)
-});
+    console.log(`Listening to port ${PORT}`)
+})
