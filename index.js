733fbf3cbac8ad508ec6a16e6d0a278134ce5c5e
Passapol <110749961+passapol146990@users.noreply.github.com>
2023-08-29 14:02:57 +0700

    template

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 5000;
 
+
+
 app.get("/", (req, res) => {
+
     res.send('<a href="/auth/google">Authentication With Google </a>')
 });
 
